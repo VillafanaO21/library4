81f946add1480bab4cd6691e03f62ed2acf5d6fb
rhsb <ovillafana65@gmail.com>
2023-02-08 11:37:51 -0800

    1 commit

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,4 +1,5 @@
 const {Course} = require('../models')
+const departments = ['Math', 'English', 'Music', 'Art', 'PE', 'WorldLanguage', 'Social Studies', 'Science'].sort();
 
 //view all
 module.exports.viewAll = async function (req,res) {
@@ -7,17 +8,60 @@ module.exports.viewAll = async function (req,res) {
 }
 
 //profile
-module.exports.viewAll= async function(req,res){
+module.exports.viewProfile= async function(req,res){
     const course = await Course.findByPk(req.params.id);
     res.render('courses/profile', {course})
 }
 
 //render add form
+module.exports.renderAddForm = function(req, res){
+    const course = {
+        name:'',
+        departments: department[0],
+        instructor_name: '',
+        description:''
+    }
+    res.render('courses/add', {course, department});
+}
 
 //add
+module.exports.addCourse = async function(req, res){
+    const course = await Course.create({
+        name:req.body.name,
+        departments:req.body.department,
+        instructor_name: req.body.instructor_name,
+        description:req.body.description
+    });
+    res.redirect(`/courses/profile/${course.id}`);
+}
 
 //render edit form
+module.exports.renderEditForm = async function(req,res){
+    const course = await Course.findByPk(req.params.id);
+    res.render('courses/edit', {course,departments});
+}
 
 //update
+module.exports.updateCourses = async function(req, res){
+    const course = await Course.update({
+        name:req.body.name,
+        departments:req.body.department,
+        instructor_name: req.body.instructor_name,
+        description:req.body.description
+    }, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/courses/profile/${req.params.id}`);
+}
 
 //delete
+module.exports.deleteCourse = async function(req, res){
+    await Course.destroy({
+        where: {
+            id:req.params.id
+        }
+    });
+    res.redirect('/courses');
+}
--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/courses', courseController.viewAll);
-router.get('/courses/profile/:id', courseController.viewAll)
+
+router.get('/courses/profile/:id', courseController.viewProfile)
+
+router.get('/courses/edit/:id', courseController.renderEditForm);
+
+router.post('/courses/edit/:id', courseController.updateCourses);
+
+router.get('/courses/add', courseController.renderAddForm);
+
+router.post('/courses/add', courseController.addCourse);
+
+router.get('/courses/delete/:id', courseController.deleteCourse);
+
 module.exports = router;
